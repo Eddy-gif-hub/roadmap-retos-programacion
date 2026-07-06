b489af5db51b140211522dc49b98d5638412db2d
Eddy Javier Madrid Rosero <edmadridro@uide.edu.ec>
2026-07-06 03:07:16 +0000

    wip: First work of programming logic

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/eddy_m.py
@@ -0,0 +1,33 @@
+#Primer ejercicio 00 de MOuredev
+
+
+#--- primer ejercicio---
+# https:python.org
+
+#--- segundo ejercicio ---
+# Comentarion en una línea
+
+""" ES un comentario para varias lineas
+    como se puede observar 
+    por orden"""
+    
+#--- tercer ejercicio ---
+variable = "es un string"
+
+CONSTANTE = "es global" # Se usa fuera de una funcion
+
+#--- cuarto ejercicio ---
+enteros = 25
+decimales = 25.25
+cadena_texto = "Es mi vida bella"
+booleanos = True
+
+#--- quinto ejercicio ---
+nombre_lenguaje = "Python"
+print("!Hola, ", nombre_lenguaje)
+
+#--- quinto ejercicio ---
+print(type(enteros))
+print(type(decimales))
+print(type(cadena_texto))
+print(type(booleanos))
